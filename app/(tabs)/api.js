209593963c94b50a48ae948b1886bfe06c50da5c
mariaeandrade <dudadeandrade13@gmail.com>
2026-09-21 11:27:26 -0300

    arruma um erro

--- app/(tabs)/api.js
+++ app/(tabs)/api.js
@@ -61,7 +61,7 @@ export default function FilmesListarScreen() {
             setCarregando(false)
         }
     }
-// faz c que isso rode so uma vez
+
     useEffect(() => {
         buscarFilmes()
     }, [])
